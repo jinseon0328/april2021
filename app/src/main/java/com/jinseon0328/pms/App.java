599547004f91827c71a1809a7208c8405e480d90
jinseon0328 <wishofsun@daum.net>
2021-05-01 16:00:30 +0900

    2021-05-01

--- app/src/main/java/com/jinseon0328/pms/App.java
+++ app/src/main/java/com/jinseon0328/pms/App.java
@@ -2,7 +2,8 @@ package com.jinseon0328.pms;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }
